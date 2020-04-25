d1fa3e693a910c7fbcc99324ed9d6799fe8a8eda
geophf <dauclair@hotmail.com>
2020-04-25 14:08:17 -0400

    Simplified rev-with-append, O(2n), solution to rev1 solution. Added description.

--- prolog/p99/p05_rev.pl
+++ prolog/p99/p05_rev.pl
@@ -3,50 +3,16 @@
 
 P05 (*) Reverse a list.
 
- */
-
 rev([], []).
 rev(X, [H|T]) :-
   rev(Y, T),
   append(Y, [H], X).
 
-/* The above is in O(2n)-time. Is there a linear algorithm to reverse a list? */
+The above is in O(2n)-time. Is there a linear algorithm to reverse a list?
+How about this?
+*/
 
+rev(X, List) :- rev1(X, [], List).
 
-
-/* --------------------------------------------------------------
- * p06_palindrome.pl
- */
-
-/* :- [p05_rev]. */
-
-palindrome(X) :- rev(X, X).
-
-
-/* --------------------------------------------------------------
- * p07_flatten.pl
- */
-
-flatten([], []).
-flatten([H|T], X) :-
-  (is_list(H) -> flatten(H, Y); Y = [H]),
-  flatten(T, Z), append(Y, Z, X).
-
-is_list([]).
-is_list([_|_]).
-
-
-/* --------------------------------------------------------------
- * p08_compress.pl
- */
-
-compress([], []).
-compress([H|T],[H|X]) :-
-   skip(H, T, Z),
-   compress(Z, X).
-
-skip(_, [], []).
-skip(X, [X|T], Z) :-
-   skip(X, T, Z).
-skip(X, [H|T], [H|T]) :-
-   not H = X.
+rev1(Ans, Ans, []).
+rev1(X, Ans, [H|T]) :- rev1(X, [H|Ans], T).
