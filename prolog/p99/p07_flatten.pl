6ef617076bd1c569e2a7d657a39b79aa8deb7cdd
geophf <dauclair@hotmail.com>
2020-04-25 18:15:47 -0400

    Corrected comments (formatting and word-choice)

--- prolog/p99/p07_flatten.pl
+++ prolog/p99/p07_flatten.pl
@@ -2,7 +2,8 @@
  * p07_flatten.pl
 
 P07 (**) Flatten a nested list structure.
-Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+Transform a list, possibly holding lists as elements into a `flat' list by 
+replacing each list with its elements (recursively).
 
 Example:
 ?- my_flatten([a, [b, [c, d], e]], X).
@@ -15,13 +16,13 @@ flatten([H|T], X) :-
   (is_list(H) -> flatten(H, Y); Y = [H]),
   flatten(T, Z), append(Y, Z, X).
 
-Again, this algorithm is in double-linear, or more line m-by-n time. Let's
+Again, this algorithm is in double-linear, or more like m-by-n time. Let's
 do something better with definite clause grammars.
 
 Recall, that, since we're using difference lists, we work 'backward' to our
 answer.
 
-... I'm not saying that everywhere you're using append/3 that you should use
+Also, I'm not saying that everywhere you're using append/3 that you should use
 difference lists, otherwise you're doing it wrong, but ... yeah. This
 "iteratively constructing a list with append/3" once bit me in production. It
 worked for small lists in test, but when we moved beyond 1000s of element,
