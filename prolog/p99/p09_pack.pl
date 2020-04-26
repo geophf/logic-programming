9a595240e33de63459b819cb7a0623a9699159b7
geophf <dauclair@hotmail.com>
2020-04-26 02:14:07 -0400

    Added problem descriptions; p10 converted to a DCG

--- prolog/p99/p09_pack.pl
+++ prolog/p99/p09_pack.pl
@@ -1,3 +1,14 @@
+/*
+P09 (**) Pack consecutive duplicates of list elements into sublists.
+
+If a list contains repeated elements they should be placed in separate sublists.
+
+Example:
+
+?- pack([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+X = [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
+*/
+
 pack([], []).
 pack([H|T], [Hs|Y]) :-
    collect(H, T, Hs, Ts),
--- prolog/p99/p10_encode.pl
+++ prolog/p99/p10_encode.pl
@@ -1,9 +1,25 @@
-:- [ps]. /* p04_len */
+:- [p04_len].
 :- [p09_pack].
+
+/*
+P10 (*) Run-length encoding of a list.
+Use the result of problem P09 to implement the so-called run-length encoding 
+data compression method. Consecutive duplicates of elements are encoded as 
+terms [N,E] where N is the number of duplicates of the element E.
+
+Example:
+
+?- encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+X = [[4,a],[1,b],[2,c],[2,a],[1,d][4,e]]
 
 encode(List, X) :-
    pack(List, Y),
    encode1(Y, X).
+
+or, as a DCG:
+*/
+
+encode --> pack, encode1.
 
 encode1([], []).
 encode1([H|T], [H1|T1]) :-
