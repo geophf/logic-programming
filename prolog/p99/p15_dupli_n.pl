6d1b2cbbc282d3b84e396b73f4c4493486700d1b
geophf <dauclair@hotmail.com>
2020-04-26 21:25:16 -0400

    Use the utils/list repeat/3 library-function, switch dupli to DCG-style

--- prolog/p99/p15_dupli_n.pl
+++ prolog/p99/p15_dupli_n.pl
@@ -1,3 +1,5 @@
+:- ['utils/list'].
+
 /*
 P15 (**) Duplicate the elements of a list a given number of times.
 
@@ -8,7 +10,6 @@ X = [a,a,a,b,b,b,c,c,c]
 
 What are the results of the goal:
 ?- dupli(X,3,Y).
-*/
 
 dupli([],_,[]).
 dupli([H|T],N,List) :-
@@ -16,8 +17,21 @@ dupli([H|T],N,List) :-
    dupli(T,N,Ts),
    append(Hs, Ts, List).
 
+Let's use difference lists for dupli to avoid the append/3-mxn-list traversals.
+*/
+
+dupli(List,N,Ans) :- dupli1(List, N, Ans, []).
+
+dupli1([],_) --> [].
+dupli1([H|T],N) --> 
+   { repeat(N,H,Hs) },
+   Hs,
+   dupli1(T,N).
+
+/*
 rep(_,0,[]).
 rep(C,N,[C|List]) :-
    N > 0,
    M is N - 1,
    rep(C,M,List).
+*/
