e458b458ad365226b9860f71771fe3063fa38e80
geophf <dauclair@hotmail.com>
2020-04-28 14:33:21 -0400

    My glorious attempt to DCG-ify p19, all recorded for posterity :/

--- prolog/p99/p19_rot.pl
+++ prolog/p99/p19_rot.pl
@@ -1,3 +1,44 @@
+/*
+P19 (**) Rotate a list N places to the left.
+
+Examples:
+
+?- rotate([a,b,c,d,e,f,g,h],3,X).
+X = [d,e,f,g,h,a,b,c]
+
+?- rotate([a,b,c,d,e,f,g,h],-2,X).
+X = [g,h,a,b,c,d,e,f]
+
+Hint: Use the predefined predicates length/2 and append/3, as well as the result
+of problem P17.
+
+[original code]
+
+... the above is the original solution. CAN WE DO BETTER???
+
+I tried the below, but NOPE!
+
+rotate(List,0,List).
+rotate(List,N,Ans) :- rotate1(N,List,Ans,List).
+
+rotate1(0,_) --> [].
+rotate1(N,[H|T]) -->
+   { N > 0,
+     M is N - 1 },
+   rotate3(M,[H|T]).
+
+rotate3(M,[H|T],Ans,_) :- rotate4(M,[H|T],Ans,[]).
+
+rotate4(M,[H|T]) -->
+   T,
+   [H],
+   rotate2(M).
+
+rotate2(N,Ans,List) :- rotate(List,N,Ans).
+
+Ugh. I got three of my list, rotated once, as the answer. So ... no?
+*/
+
 rotate(List,0,List).
 rotate(List,N,X) :-
    N > 0,
@@ -12,3 +53,15 @@ rotate(List,N,X) :-
 
 rot1([H|T],List) :-
    append(T, [H], List).
+
+/*
+... so let's try to replace the append in rot1/2
+
+rot1([H|T],List) :- rot2([H|T],List,[]).
+
+rot2([H|T]) --> T, [H].
+
+... no, this works for the positive rotation, but not for negate rotation.
+
+BAH! We use append/3 now! :/
+*/
