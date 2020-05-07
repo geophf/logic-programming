1c00292d3fbed714159302b184d3e01e71b100aa
geophf <dauclair@hotmail.com>
2020-05-07 16:09:11 -0400

    I ... LOVE ... functional programming, ICYMI

---
+++ prolog/p99/p39_list_primes.pl
@@ -0,0 +1,18 @@
+/*
+P39 (*) A list of prime numbers.
+
+Given a range of integers by its lower and upper limit, construct a list of all 
+prime numbers in that range.
+*/
+
+:- ['utils/list'].
+:- ['utils/math'].
+
+list_primes(Start, End, Primes) :-
+   range(Start, End, Range),
+   filter(is_prime, Range, Primes).
+
+/*
+?- list_primes(2, 100, Primes).
+Primes=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+*/
