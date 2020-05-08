2a55affc88339fd96e6d56b89ea4088aa385600a
geophf <dauclair@hotmail.com>
2020-05-07 21:13:03 -0400

    Added functional comparators.

--- prolog/utils/cat.pl
+++ prolog/utils/cat.pl
@@ -13,6 +13,10 @@ apply1([H|T]) -->
    apply1(T).
 
 Now. How do we do compose? 
+
+compose(G, F, A0, A) :-
+   call(G, A0, A1),
+   apply(F, A1, A).
 */
 
 /* a better apply. This one doesn't bind free variables */
@@ -21,12 +25,6 @@ apply(CurriedFn, Arg, FnArg) :-
    CurriedFn =.. List,
    append(List, [Arg], ListFn),
    FnArg =.. ListFn.
-
-/*
-compose(G, F, A0, A) :-
-   call(G, A0, A1),
-   apply(F, A1, A).
-*/
 
 /* ---------------------------------- tuples ----------------------------- */
 
@@ -40,6 +38,17 @@ dup(X, X - X).
 
 /* ---------------------------------- Arrows ---------------------------- */
 
+/*
+no, but trying:
 '&&&'(F, G, X - Y, FX - GY) :-
    apply(F, X, FX),
    apply(G, Y, GY).
+*/
+
+/* --------------------------------- Comparators ------------------------ */
+
+lt(V1, V2) :- V2 < V1.
+gt(V1, V2) :- V2 > V1.
+eq(V1, V1).
+le(V1, V2) :- V2 =< V1.
+ge(V1, V2) :- V2 >= V1.
