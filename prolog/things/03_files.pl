4ba8f6c3dbf2dba9591e66b5d1adfb62f3f96c51
geophf <dauclair@hotmail.com>
2020-05-16 07:44:00 -0400

    I need more more things. Like file-y things. Those things.

---
+++ prolog/things/03_files.pl
@@ -0,0 +1,22 @@
+/*
+DIS HEAHE IS FILE IO!
+
+But, since we're on the cloud, and we're lambda-ing it up, or whatever,
+let's also consider 'files' as stream, i.e.: S3 or some URL.
+
+And also, we have to consider REST calls, eventually, don't we?
+
+Yes. Yes, we do.
+
+So, anyway: files FRIST!
+
+read_sommat_from_dis_heahe_file_n_stupfen :-
+   see(dis),
+   do(dat),
+   seen.
+
+... right?
+*/
+
+yo.
+foo.
