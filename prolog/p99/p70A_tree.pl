79a05a2968cb907051558f3b379111da8cbbd792
geophf <dauclair@hotmail.com>
2020-05-23 00:31:53 -0400

    Start of building a tree from a string

---
+++ prolog/p99/p70A_tree.pl
@@ -0,0 +1,24 @@
+/*
+P70 (**) Tree construction from a node string
+
+We suppose that the nodes of a multiway tree contain single characters. In the 
+depth-first order sequence of its nodes, a special character ^ has been 
+inserted whenever, during the tree traversal, the move is a backtrack to the 
+previous level.
+
+By this rule, the tree in the figure below is represented as: afg^^c^bd^e^^^
+
+Define the syntax of the string and write a predicate tree(String,Tree) to 
+construct the Tree when the String is given. Work with atoms (instead of 
+strings). Make your predicate work in both directions. [ed: no]
+
+    (a)
+   / | \
+(f) (c) (b)
+ |     /   \
+(g) (d)     (e)
+*/
+
+tree(Atom, t(H, Nodes)) :-
+   name(Atom, [H|T]),
+   parse_tree(T, Nodes)
