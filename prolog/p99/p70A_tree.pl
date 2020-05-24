b84746dc29466e4f5a8d134933938c02a8bd09db
geophf <dauclair@hotmail.com>
2020-05-23 23:21:28 -0400

    A multiway tree #DataStructure, constructed using 'carat-notation' in Prolog

--- prolog/p99/p70A_tree.pl
+++ prolog/p99/p70A_tree.pl
@@ -17,8 +17,74 @@ strings). Make your predicate work in both directions. [ed: no]
 (f) (c) (b)
  |     /   \
 (g) (d)     (e)
-*/
 
 tree(Atom, t(H, Nodes)) :-
    name(Atom, [H|T]),
    parse_tree(T, Nodes)
+
+No, I've got it. The (^) operator is very stack-y. So we'll pass in the
+tree being worked on as history, the most recent tree being at the top of
+the stack.
+
+Now, [H|T] is a LIFO ('last in/first out') stack, so ... we're done.
+
+Also, "^" is [94]
+*/
+
+:- ['library/tree'].
+
+carat(94).
+
+tree(Atom, Tree) :-
+   name(Atom, [H|T]),
+   name(A, [H]),
+   tree_string(T, [t(A, [])], Tree).
+
+tree_string([], [Tree], Tree). % ... should be at the top of the stack now.
+tree_string([H|T], Trees, Tree) :-
+   carat(H) ->
+      pop_n_go(T, Trees, Tree)
+   ;
+      name(A, [H]),
+      tree_string(T, [t(A, [])|Trees], Tree).
+
+pop_n_go([], [Tree], Tree).
+pop_n_go(Str, [T|[t(A, Kids)|Trees]], Tree) :-
+   tree_string(Str, [t(A, [T|Kids])|Trees], Tree).
+
+/*
+So:
+
+?- tree('afg^^c^bd^e^^^', Tree), ppt(Tree).
+Tree=t(a,[t(b,[t(e,[]),t(d,[])]),t(c,[]),t(f,[t(g,[])])])
+
+                                   t
+                                   |
+            .------------------------.
+            |                        |
+            a                        .
+                                     |
+                        .----------------------.
+                        |                      |
+                        t                      .
+                        |                      |
+               .----------.           .------------.
+               |          |           |            |
+               b          .           t            .
+                          |           |            |
+                    .--------.      .---.        .--------.
+                    |        |      |   |        |        |
+                    t        .      c  []        t       []
+                    |        |                   |         
+                  .---.    .-----.         .------.
+                  |   |    |     |         |      |
+                  e  []    t    []         f      .
+                           |                      |
+                         .---.                  .-----.
+                         |   |                  |     |
+                         d  []                  t    []
+                                                |      
+                                              .---.
+                                              |   |
+                                              g  []
+*/
