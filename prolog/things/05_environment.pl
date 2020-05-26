20b94b48dadec2684b4ef00aa76e225ee29b551f
geophf <dauclair@hotmail.com>
2020-05-26 11:53:06 -0400

    grabbing environment variables

---
+++ prolog/things/05_environment.pl
@@ -0,0 +1,14 @@
+/*
+Besides from the command-line we ... maybe? ... need to read from the 
+environment?
+
+We can certainly pass in the environment on the command-line as context. That's
+one way of going about it, and certainly cloud-y in the AWS-sense, but there
+also will be cases where we store stuff (like: the database connection-string)
+in the environment that we need to snarf before we proceed.
+
+Get that environment: just get it!
+*/
+
+foo.
+bar.
