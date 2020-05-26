d5b0ecaea7f762a81d3ac36e26b76af6dc624d51
geophf <dauclair@hotmail.com>
2020-05-26 12:21:17 -0400

    We're now reading environmental variables

--- prolog/things/05_environment.pl
+++ prolog/things/05_environment.pl
@@ -10,5 +10,28 @@ in the environment that we need to snarf before we proceed.
 Get that environment: just get it!
 */
 
-foo.
-bar.
+:- initialization(main, main).
+
+:- ['utils/list'].
+:- ['utils/io'].
+
+main(_) :-
+   write('Connection arguments to our Prolog Graph data store:'), nl, nl,
+   graph_connect_info(User, Pass, Endpoint),
+   zip(['PROLOG_GRAPH_USER','PROLOG_GRAPH_PASS','PROLOG_GRAPH_ENDPOINT'],
+       [User, Pass, Endpoint], Infos),
+   do(write_term, Infos).
+   
+graph_connect_info(User, Pass, Endpoint) :-
+   getenv('PROLOG_GRAPH_USER', User),
+   getenv('PROLOG_GRAPH_PASS', Pass),
+   getenv('PROLOG_GRAPH_ENDPOINT', Endpoint).
+
+/*
+$ ./envs yo                     
+Connection arguments to our Prolog Graph data store:
+
+'PROLOG_GRAPH_USER'-sumthin
+'PROLOG_GRAPH_PASS'-sumthin_else
+'PROLOG_GRAPH_ENDPOINT'-sumwherez
+*/
