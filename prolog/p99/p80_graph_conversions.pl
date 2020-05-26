924999193c763bb53dbc50e93e8909a4a2d13132
geophf <dauclair@hotmail.com>
2020-05-26 16:39:46 -0400

    All-ways conversions for simple graph forms.

--- prolog/p99/p80_graph_conversions.pl
+++ prolog/p99/p80_graph_conversions.pl
@@ -77,18 +77,148 @@ NonDups=[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c)]
 yes
 */
 
-edge_clause2graph_term(graph(Nodes, Edges)) :-
-   edges(E0),
-   all_nodes(E0, Nodes),
+edge_clause2graph_term(Graph) :-
+   edges(Edges),
+   edge_clause_list2graph_term(Edges, Graph).
+
+edge_clause_list2graph_term(Edges, graph(Nodes, Edges1)) :-
+   all_nodes(Edges, Nodes),
 
    % now we can remove the edge(d,d) or we can keep it. 
    % The graph form removes it.
 
-   remove_idem_edges(E0, Edges).
+   remove_idem_edges(Edges, Edges1).
 
 /*
 ?- edge_clause2graph_term(Graph).
 Graph=graph([b,c,d,f,g,h,k],[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c)])
 
 yes
+
+... backwards doesn't halt.
+*/
+
+adjacent_nodes_to(Edges, Node, n(Node, Nexts)) :-
+   ant(Edges, Node, Nexts, []).
+
+ant([], _) --> [].
+ant([edge(A, B)|Edges], N) -->
+   ({ N = A, C = B ; N = B, C = A } -> [C] ; []),
+   ant(Edges, N).
+
+/*
+?- edges(Edges), adjacent_nodes_to(Edges, b, Node).
+Edges=[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c),edge(d,d)],
+Node=n(b,[f,c])
+
+yes
+*/
+
+edge_clause2adjacency_list(Adjays) :-
+   edges(Edges),
+   edge_clause_list2adjaceny_list(Edges, Adjays).
+
+edge_clause_list2adjaceny_list(Edges, Adjays) :-
+   all_nodes(Edges, Nodes),
+   remove_idem_edges(Edges, E1),
+   map(adjacent_nodes_to(E1), Nodes, Adjays).
+
+/*
+?- edge_clause2adjacency_list(Adjs).
+Adjs=[n(b,[f,c]),n(c,[f,b]),n(d,[]),n(f,[k,b,c]),n(g,[h]),n(h,[g]),n(k,[f])]
+
+yes
+
+... backwards doesn't halt.
+*/
+
+edge_clause2human_readable(Human) :-
+   edges(Edges),
+   edge_clause_list2human_readable(Edges, Human).
+
+edge_clause_list2human_readable(Edges, Human) :-
+   map(detup_retup, Edges, Human).
+
+detup_retup(edge(A, B), A - B).
+detup_retup(edge(X, X), X).
+
+/*
+?- edge_clause2human_readable(Human).
+Human=[h-g,k-f,f-b,f-c,b-c,d];
+
+Human=[h-g,k-f,f-b,f-c,b-c,d-d];
+
+no
+
+also:
+
+?- edge_clause_list2human_readable(E, [h-g,k-f,f-b,f-c,b-c,d]).
+E=[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c),edge(d,d)]
+
+yes
+
+YAY!
+
+So, with that, we can go from human_readable to graph_term thus:
+
+?- edge_clause_list2human_readable(E, [h-g,k-f,f-b,f-c,b-c,d]), 
+   edge_clause_list2graph_term(E, G).
+E=[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c),edge(d,d)],
+G=graph([b,c,d,f,g,h,k],[edge(h,g),edge(k,f),edge(f,b),edge(f,c),edge(b,c)])
+
+Voilà.
+*/
+
+human_readable2graph_term(HR, G) :-
+   edge_clause_list2human_readable(E, HR),
+   edge_clause_list2graph_term(E, G).
+
+/*
+Going from graph_term to human_readable requires a bit more work, because
+we need to go 'backwards' to the edge_clause_list form from graph_term.
+*/
+
+graph_term2edge_clause_list(Graph, Edges) :- g2e(Graph, Edges, []).
+
+g2e(graph(Nodes, Edges)) -->
+   map_n_delete(Edges, Nodes, RemainingNodes),
+   mk_edges(RemainingNodes).
+
+mk_edges([]) --> [].
+mk_edges([H|T]) -->
+   [edge(H, H)],
+   mk_edges(T).
+
+map_n_delete([], Ns, Ns) --> [].
+map_n_delete([e(A, B)|T], Ns, Nodes) -->
+   { delete(A, Ns, N0),
+     delete(B, N0, N1) },
+   [edge(A, B)],
+   map_n_delete(T, N1, Nodes).
+
+/*
+?- graph_term2edge_clause_list(graph([b,c,d,f,g,h,k],
+                                     [e(b,c),e(b,f),e(c,f),e(f,k),e(g,h)]),
+                               Edges).
+Edges=[edge(b,c),edge(b,f),edge(c,f),edge(f,k),edge(g,h),edge(d,d)]
+
+yes
+
+Woot!
+
+And with that, we can go from graphs to human-readable form thusly:
+*/
+
+graph_term2human_readable(G, HR) :-
+   graph_term2edge_clause_list(G, E),
+   edge_clause_list2human_readable(E, HR).
+
+/*
+?- graph_term2human_readable(graph([b,c,d,f,g,h,k],
+                                   [e(b,c),e(b,f),e(c,f),e(f,k),e(g,h)]), HR).
+HR=[b-c,b-f,c-f,f-k,g-h,d-d]
+
+yes
+
+TADA! And that's the all ways conversions for the simple graph forms.
 */
