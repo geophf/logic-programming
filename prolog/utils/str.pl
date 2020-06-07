a2cd62190052a1e668e0998715ece21556a87132
geophf <dauclair@hotmail.com>
2020-06-06 23:18:05 -0400

    Added list_str/2 and list_str_with/3. Using 'str'-label throughout.

--- prolog/utils/str.pl
+++ prolog/utils/str.pl
@@ -2,18 +2,18 @@
 
 :- ['utils/list'].
 
-capitalize_string(Hay, Yay) :-
+capitalize_str(Hay, Yay) :-
    string_codes(Hay, [H|Ay]),
    atom_codes(Huh, [H]),
    upcase_atom(Huh, J),
    atom_codes(J, [Y]),
    string_codes(Yay, [Y|Ay]).
 
-capitalize_atom2string(Lower, Upper) :-
+capitalize_atom2str(Lower, Upper) :-
    atom_chars(Lower, Str),
-   capitalize_string(Str, Upper).
+   capitalize_str(Str, Upper).
 
-str_cats(Strings, String) :-
+str_cat(Strings, String) :-
    reduce(rev_str_cat, "", Strings, String).
 
 rev_str_cat(A, B, C) :- string_concat(B, A, C).
@@ -24,3 +24,13 @@ rev_str_cat_with(Str, A, B, C) :-
 
 str_cat_with(Sep, [S|Trs], Str) :-
    reduce(rev_str_cat_with(Sep), S, Trs, Str).
+
+list_str(List, String) :-
+   list_str_with("[]", List, String).
+
+list_str_with(Brackets, List, String) :-
+   string_codes(Brackets, [L, R]),
+   string_codes(LBr, [L]),
+   string_codes(RBr, [R]),
+   str_cat_with(", ", List, Lost),
+   str_cat([LBr, Lost, RBr], String).
