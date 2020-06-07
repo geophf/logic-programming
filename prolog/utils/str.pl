0ce73b594fa8a14185e000e01b6166b937f86cae
geophf <dauclair@hotmail.com>
2020-06-07 01:06:37 -0400

    Added words/2

--- prolog/utils/str.pl
+++ prolog/utils/str.pl
@@ -10,7 +10,7 @@ capitalize_str(Hay, Yay) :-
    string_codes(Yay, [Y|Ay]).
 
 capitalize_atom2str(Lower, Upper) :-
-   atom_chars(Lower, Str),
+   atom_string(Lower, Str),
    capitalize_str(Str, Upper).
 
 str_cat(Strings, String) :-
@@ -22,6 +22,7 @@ rev_str_cat_with(Str, A, B, C) :-
    string_concat(Str, A, Z),
    string_concat(B, Z, C).
 
+str_cat_with(_, [], "").
 str_cat_with(Sep, [S|Trs], Str) :-
    reduce(rev_str_cat_with(Sep), S, Trs, Str).
 
@@ -34,3 +35,35 @@ list_str_with(Brackets, List, String) :-
    string_codes(RBr, [R]),
    str_cat_with(", ", List, Lost),
    str_cat([LBr, Lost, RBr], String).
+
+words(Str, Words) :-
+   string_codes(Str, Codes),
+   ws(Codes, Words, []).
+
+space(32).
+space(C) :- C - 9 =< 4.
+space(160).
+
+/* ---- Helper predicates for words/2 ---- */
+
+ws([]) --> [].
+ws([C|Odes]) -->
+   { next_word([C|Odes], Cs, Codes, []),
+     string_codes(Word, Codes) },
+   [Word],
+   ws(Cs).
+
+next_word([], []) --> [].
+next_word([C|Odes], NewCodes) -->
+   { space(C) } ->
+      next_word(Odes, NewCodes)
+   ;
+      word([C|Odes], NewCodes).
+
+word([], []) --> [].
+word([C|Odes], NewCodes) -->
+   { space(C) } ->
+      { NewCodes = [C|Odes] }
+   ;
+      [C],
+      word(Odes, NewCodes).
