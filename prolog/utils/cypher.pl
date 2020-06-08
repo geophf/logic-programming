870aba356300bcbdb262872dd5ab9ca169c7bffd
geophf <dauclair@hotmail.com>
2020-06-08 18:33:49 -0400

    Improved thunk-predicate for nodes and relations with and without attributes

--- prolog/utils/cypher.pl
+++ prolog/utils/cypher.pl
@@ -21,18 +21,20 @@ materialize_cyphers(Graph) -->
 %    where t is the type and l1, ... ln are the labels for the data a, b, ... z
 
 node(Var, Term, Node) :-
-   thunk("()", Var, Term, Node).
+   thunk(capitalize_atom2str, "()", Var, Term, Node).
 
-thunk(Brackets, Var, Term, Thunk) :-
+thunk(Capitalizer, Brackets, Var, Term, Thunk) :-
    string_codes(Brackets, [L, R]),
    string_codes(LBr, [L]),
    string_codes(RBr, [R]),
    Term =.. [Type|SubTerms],
    atom_string(Var, V),
-   capitalize_atom2str(Type, Label),
+   CapFn =.. [Capitalizer, Type, Label],
+   CapFn,
    map(term2attrib, SubTerms, Attribs),
-   list_str_with("{}", Attribs, AttribList),
-   str_cat([LBr, V, ":", Label, " ", AttribList, " ", RBr], Thunk).
+   list_str_with("{}", Attribs, AttribList, nothing_on_empty),
+   padded(AttribList, Padded),
+   str_cat([LBr, V, ":", Label, Padded, RBr], Thunk).
 
 term2attrib(Term, Attrib) :-
    Term =.. [Fn, Arg],
@@ -47,23 +49,27 @@ val(Val, Str) :-
      str_cat(["\"", Val, "\""], Str).
 
 create_node(Term, Stmt) :-
+   create_node1(Term, semicolon, Stmt).
+
+create_node1(Term, Semiwhat, Stmt) :-
+   (Semiwhat = semicolon -> Semi = ";" ; Semi = ""),
    node(a, Term, Node),
-   str_cat(["CREATE ", Node, "; "], Stmt).
+   str_cat(["CREATE ", Node, Semi, " "], Stmt).
 
 match_node(Var, Term, Stmt) :-
    node(Var, Term, Node),
    str_cat(["MATCH ", Node, " "], Stmt).
    
 node_ref(A, Ref) :-
-   list_str_with("()", [A], Ref).
+   list_str_with("()", [A], Ref, _).
 
 % Like node/3, rel/4 takes a term describing its attributes
 
 rel(A, Rel, B, Relation) :-
    node_ref(A, ARef),
    node_ref(B, BRef),
-   thunk("[]", r, Rel, RelStr),
-   str_cats([ARef, "-", RelStr, "->", BRef], Relation).
+   thunk(string_upper, "[]", r, Rel, RelStr),
+   str_cat([ARef, "-", RelStr, "->", BRef], Relation).
 
 merge_relation(Term, Edge, Stmt) :-
    unpair(Edge, A, B),
